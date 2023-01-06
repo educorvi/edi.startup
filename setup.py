eebc4f872bbd09aca70994468c7645ec8c3d2610
Lars Walther <Lars Walther>
2023-01-06 13:23:04 +0000

    python_requires in setup.py gefixt

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.7",
+    python_requires=">=3.8, >=3.9, >=3.10",
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
